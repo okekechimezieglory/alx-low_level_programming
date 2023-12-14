fe7cf7356e6201f57dfb1e4d78443863f302c184
okekechimezieglory <okekechimezieglory@gmail.com>
2023-12-14 08:11:47 -0800

    reduce characters using a variable

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <string.h>
 #include <unistd.h>
 /**
  * main - main entry
@@ -7,10 +6,7 @@
  */
 int main(void)
 {
-	int = 59;
-
 	write(2,
-	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-	      s);
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
