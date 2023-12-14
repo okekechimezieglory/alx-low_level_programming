c17dde9b05bbbef1df0496ce45b0b86207456c0e
okekechimezieglory <okekechimezieglory@gmail.com>
2023-12-14 08:06:55 -0800

    reduce characters using a variable

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	int s = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	int = 59;
 
 	write(2,
 	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
