5d63ffda2f1b2504c1b0c46416e682bad2ee630d
okekechimezieglory <okekechimezieglory@gmail.com>
2023-12-20 11:18:49 -0800

    0-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include "stdio.h"
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Print "_putchar" followed by a new line.
+ * Description: You are not allowed to include standard libraries.
+ * Return: 0
+ */
+int main(void){
+	printf("_putchar\n");
+	return(0);
+}
