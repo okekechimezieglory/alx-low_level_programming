4cef89cba6562d96c1edc378157e96ce1d820adf
okekechimezieglory <okekechimezieglory@gmail.com>
2024-02-01 15:41:54 -0800

    changes

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+
+
+/**
+* myStartupFun - Apply the constructor attribute to myStartupFun() so that it
+*   is executed before main()
+*/
+void myStartupFun(void) __attribute__((constructor));
+
+
+/**
+* myStartupFun - implementation of myStartupFun
+*/
+void myStartupFun(void)
+{
+   printf("You're beat! and yet, you must allow,\n");
+   printf("I bore my house upon my back!\n");
+}
+
+
+// 101-hello_holberton.asm
+
+
+extern printf; the C function, to be called
+
+
+      section.data; Data section, initialized variables
+msg:    db "Hello, Holberton", 0; C string needs 0
+fmt:    db "%s", 10, 0; The printf format, "\n", '0'
+
+
+       section.text; Code section.
+
+
+   global main; the standard gcc entry point
+   main:; the program label for the entry point
+   push    rbp     ; set up stack frame, must be alligned
+
+
+   mov rdi,fmt
+   mov rsi,msg
+   mov rax,0; or can be xor rax, rax
+   call printf; Call C function
+
+
+   pop rbp; restore stack
+
+
+   mov rax,0; normal, no error, return value
+   ret; return
