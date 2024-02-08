8d4f5bde19294fc567ffa1f027eeec9869415e84
okekechimezieglory <okekechimezieglory@gmail.com>
2024-02-08 00:49:22 -0800

    0-print_listint

--- 0x13-more_singly_linked_list/0-print_listint.c
+++ 0x13-more_singly_linked_list/0-print_listint.c
@@ -8,11 +8,14 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	const listint_t *c;
-	size_t count;
+	size_t num = 0;
 
-	for (c = h, count = 0; c != NULL; count++, c = c->next)
-		printf("%d\n", c->n);
+	while (h)
+	{
+	printf("%d\n", h->n);
+	num++;
+	h = h->next;
+	}
 
-	return (count);
+	return (num);
 }
